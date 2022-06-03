f7d95cc67f94ad91f3de91be06f0fa5117f01ce2
Ryan M Smith <rms1000watt@gmail.com>
2022-06-02 17:27:13 -0700

    wip

--- http-client/src/main.rs
+++ http-client/src/main.rs
@@ -1,5 +1,19 @@
-// use http::{Request, Reponse}
+use reqwest;
 
 fn main() {
-    println!("Hello, world!");
+  println!("Hello, world!");
+
+  let res = reqwest::blocking::get("https://www.rust-lang.org");
+  if res.is_err() {
+    println!("found error");
+    return;
+  }
+
+
+
+  // println!("body = {:?}", body);
+
+  // let resp = reqwest::blocking::get("https://httpbin.org/ip")?.text()?;
+  // println!("{:#?}", resp);
+  // Ok(())
 }
