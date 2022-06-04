7f45de5eb4afad9161bf89c86136ceeab1e45bf6
Ryan M Smith <rms1000watt@gmail.com>
2022-06-03 22:28:39 -0700

    playing around

---
+++ http-server-2/src/main.rs
@@ -0,0 +1,28 @@
+use actix_web::{get, web, App, HttpServer, Responder};
+
+#[get("/hello/{name}")]
+async fn greet(name: web::Path<String>) -> impl Responder {
+    format!("hello {name}!")
+}
+
+#[get("/hi/{name}")]
+async fn greet2(name: web::Path<String>) -> impl Responder {
+    format!("hi {name}!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    // TODO: cli tool for passing in args
+
+    let bind = match HttpServer::new(|| App::new().service(greet).service(greet2))
+        .bind(("127.0.0.1", 8080))
+    {
+        Ok(bind) => bind,
+        Err(e) => {
+            println!("failed bind {}", e);
+            return Err(e);
+        }
+    };
+
+    return bind.workers(2).run().await;
+}
